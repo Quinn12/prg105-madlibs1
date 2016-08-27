6fc484b40455230666d25658c2270f16aef8bfac
Quinn12 <qhastings@students.mchenry.edu>
2016-08-27 15:54:13 -0500

    Add files via upload

---
+++ Madlips.py
@@ -0,0 +1,36 @@
+star = "Planet"
+world = "Solar System"
+diamond = "emeral"
+sky = "Mall"
+sun = "gun"
+light = "might"
+dark = "light"
+tiny = "massive"
+he = "she"
+blue = "yellow"
+eye = "mouth"
+
+print ("Twinkle, twinkle, little " + star +",")
+print ("How I wonder where you are!")
+print ("Up above the " + world + " so high,")
+print ("Like a " + diamond + "in the sky.")
+
+print ("When the blazing " + sun + "is gone,")
+print ("When he nothing shines upon,")
+print ("Then you show your little " + light + ",")
+print ("Twinkle, twinkle, all the night.")
+
+print ("Then the traveller in the " + dark + " ")
+print ("Thanks you for your " + tiny + " sparks;")
+print (" " + he + " could not see which way to go,")
+print ("If you did not twinkle so.")
+
+print ("In the " + dark + " " + blue + " sky you keep,")
+print ("And often through my curtains peep,")
+print ("For you never shut your " + eye + "")
+print ("Till the " + sun + " is in the " + sky + ".")
+
+print ("As your bright and " + tiny + " spark")
+print ("Lights the traveller in the " + dark + ",")
+print ("Though I know not what you are,")
+print ("Twinkle, twinkle, little " + star + ".")
